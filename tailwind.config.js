d38f9ddf98178493f43e649de3a6f776ccf8b6c0
Carrie-L <11453154+Carrie-L@users.noreply.github.com>
2025-09-15 15:17:04 +0800

    feat: post html artile and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+    "./_Android/*.html", 
+    "./_DSA/*.html", 
+    "./_MCP/*.html", 
+    "./_OS/*.html", 
+    "./_Others/*.html", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
